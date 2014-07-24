a2af5638c2b0716641cd3ef806b15f9ccc9b6f2d
Dmitry Teselkin <dteselkin@mirantis.com>
2014-07-24 11:13:41 +0400

    Add cmd to fetch and run the script

--- murano-devstack-kickstart.sh
+++ murano-devstack-kickstart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# wget https://raw.githubusercontent.com/dmitry-teselkin/murano-scripts/master/murano-devstack-kickstart.sh -O - | bash
+
 git clone https://github.com/stackforge/murano-deployment
 cd murano-deployment/murano-ci/scripts
 wget https://raw.githubusercontent.com/dmitry-teselkin/murano-scripts/master/murano-devstack.sh
